a340a2f32e9a6b08192630fd256fd4f3aa9b1a69
Kyu Rae Kim <kkr0913@gmail.com>
2022-12-18 23:56:05 -0500

    tested working auto

--- _Auto/Mbar_auto/Mbar_auto.ino
+++ _Auto/Mbar_auto/Mbar_auto.ino
@@ -18,18 +18,18 @@ const int MOTOR2PIN1 = 5;
 const int MOTOR2PIN2 = 18;
 const int ENABLEPIN2 = 19; 
 
-// Lidar Pins
+// Lidar XSHUT Pins
 const int LIDAR0PIN = 4;
 const int LIDAR1PIN = 0;
 const int LIDAR2PIN = 15;
 
 // Min & Max Pulse Width for Servos
 const int SERVOMIN0 = 120;
-const int SERVOMAX0 = 625;
+const int SERVOMAX0 = 610;
 const int SERVOMIN1 = 115;
-const int SERVOMAX1 = 615;
+const int SERVOMAX1 = 605;
 const int SERVOMIN2 = 120;
-const int SERVOMAX2 = 635;
+const int SERVOMAX2 = 610;
 
 // PWM Properties
 const int freq = 30000;
@@ -37,7 +37,7 @@ const int pwmChannel0 = 0;
 const int pwmChannel1 = 1;
 const int pwmChannel2 = 2;
 const int resolution = 8;
-int dutyCycle = 150;
+int dutyCycle = 180;
 
 // Servo Driver Board I2C Setup
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire);
@@ -50,33 +50,35 @@ VL53L0X_RangingMeasurementData_t measure;
 float distance;
 
 // General Variables
-const float dist_to_bar = 90;
+const float dist_to_bar = 200;
+const int SERVOMID0 = SERVOMIN0 + (SERVOMAX0 - SERVOMIN0) / 2;
+const int SERVOMID1 = SERVOMIN1 + (SERVOMAX1 - SERVOMIN0) / 2;
+const int SERVOMID2 = SERVOMIN2 + (SERVOMAX2 - SERVOMIN0) / 2;
 int track_sequence = 0;
 int bar_count = 0;
-int hook_angles[] = {SERVOMAX0, SERVOMIN1, SERVOMAX2};
+int hook_angles[] = {SERVOMID0, SERVOMIN1, SERVOMID2};
 
 // Boolean Flags
-bool shouldChangeTrack = true;
-bool baseHasArrived = false;
-bool hasOpenHook0 = false;
-bool hasOpenHook1 = true;
-bool hasOpenHook2 = false;
-bool hasDetectedBar = false;   // ground truth lidar detection
-bool isUnderPrevBar = false;       // flag to avoid closing hook right after opening it
+bool shouldChangeTrack = true;  // flag to change track sequence
+bool baseHasArrived = false;    // flag to stop base and move track
+bool hasDetectedBar = false;    // ground truth lidar detection
+bool isUnderPrevBar = true;     // flag to avoid closing hook right after opening it
 
+// Track Info Struct
 struct track {
     int motorpin1;
     int motorpin2;
     int pwmChannel;
     int servomin;
     int servomax;
-    bool hasOpenHook;
+    int servomid;
     Adafruit_VL53L0X lidar;
 };
 
-struct track track0 = {MOTOR0PIN1, MOTOR0PIN2, pwmChannel0, SERVOMIN0, SERVOMAX0, hasOpenHook0, lidar0};
-struct track track1 = {MOTOR1PIN1, MOTOR1PIN2, pwmChannel1, SERVOMIN1, SERVOMAX1, hasOpenHook1, lidar1};
-struct track track2 = {MOTOR2PIN1, MOTOR2PIN2, pwmChannel2, SERVOMIN2, SERVOMAX2, hasOpenHook2, lidar2};
+// Variables Associated with Each Track
+struct track track0 = {MOTOR0PIN1, MOTOR0PIN2, pwmChannel0, SERVOMIN0, SERVOMAX0, SERVOMID0, lidar0};
+struct track track1 = {MOTOR1PIN1, MOTOR1PIN2, pwmChannel1, SERVOMIN1, SERVOMAX1, SERVOMID1, lidar1};
+struct track track2 = {MOTOR2PIN1, MOTOR2PIN2, pwmChannel2, SERVOMIN2, SERVOMAX2, SERVOMID2, lidar2};
 track my_track;
 
 
@@ -95,7 +97,8 @@ struct track trackInfo(int track_num) {
     }
 }
 
-bool emergencyBrake() {
+// Detect the tilt with IMU
+bool isTilted() {
     JY901.receiveSerialData();
     float roll = JY901.getRoll() + 180.0;
     float pitch = JY901.getPitch();
@@ -148,14 +151,20 @@ void moveHook(int track_num, String open_close) {
     track current_track = trackInfo(track_num);
     if (open_close == "open") {
         while (hook_angles[track_num] >= current_track.servomin) {
-            hook_angles[track_num] -= 5;
+            Serial.println("opening..........");
+            hook_angles[track_num] -= 1;
             pwm.setPWM(track_num, 0, hook_angles[track_num]);
+            delay(5);
         }
+        Serial.println("opened!!!!!!!!!!!!!!!");
     } else if (open_close == "close") {
-        while (hook_angles[track_num] <= current_track.servomax) {
-            hook_angles[track_num] += 5;
+        while (hook_angles[track_num] <= current_track.servomid) {
+            Serial.println("closing..........");
+            hook_angles[track_num] += 1;
             pwm.setPWM(track_num, 0, hook_angles[track_num]);
+            delay(5);
         }
+        Serial.println("closed!!!!!!!!!!!!!!!");
     }
 }
 
@@ -200,19 +209,27 @@ void setup() {
     ledcAttachPin(ENABLEPIN0, pwmChannel0);
     ledcAttachPin(ENABLEPIN1, pwmChannel1);
     ledcAttachPin(ENABLEPIN2, pwmChannel2);
+
+    moveHook(0, "closed");
+    moveHook(1, "open");
+    moveHook(2, "closed");
 }
 
-void loop() { 
+void loop() {
     Serial.print("Sequence: "); Serial.print(track_sequence); Serial.println();
-    if (emergencyBrake()) {
+    // stop everything until the robot is stabilized
+    if (isTilted()) {
       stopAll();
       return;
     }
+    
     // defining the track sequence and opening the hook
     if (shouldChangeTrack) {
         track_sequence = (track_sequence + 1) % 3;
         my_track = trackInfo(track_sequence);
         moveHook(track_sequence, "open");
+        moveHook((track_sequence + 1) % 3, "close");
+        moveHook((track_sequence + 2) % 3, "close");
         shouldChangeTrack = false;
     }
     
@@ -220,14 +237,14 @@ void loop() {
     my_track.lidar.rangingTest(&measure, false);
     distance = measure.RangeMilliMeter;
     hasDetectedBar = (distance <= dist_to_bar);
-    Serial.print(distance); Serial.print(", "); Serial.print(hasDetectedBar); Serial.println();
 
     // move the base until current track detects next bar
     if (!baseHasArrived) {
         moveBase(track_sequence, true);
 
         if (!hasDetectedBar) isUnderPrevBar = false;                             // moved away from previous bar (update flag)
-        if (!isUnderPrevBar && hasDetectedBar) {                                 // detected the bar
+        if (!isUnderPrevBar && hasDetectedBar) {                                 // detected next bar
+            Serial.println("base has arrived");
             isUnderPrevBar = true;                                               // update the flag
             baseHasArrived = true;                                               // stop moving the base
         }
@@ -236,23 +253,23 @@ void loop() {
 
     // move current track until it reaches second bar from initial position
     if (!hasDetectedBar) {                                                       // moved away from previous bar (update flag)
-        isUnderPrevBar = false;
         Serial.println("moved away from previous bar (update flag)");
+        isUnderPrevBar = false;
     }
     if (!isUnderPrevBar && hasDetectedBar && bar_count != 1) {                   // detects the bar, but not correct one
+        Serial.println("detects the bar, but not correct one");
         bar_count += 1;
         isUnderPrevBar = true;
-        Serial.println("detects the bar, but not correct one");
     } else if (!isUnderPrevBar && hasDetectedBar && bar_count == 1) {            // detects correct bar, close hook & change track (RESET)
+        Serial.println("detects correct bar, close hook & change track (RESET)");
         stopAll();
         moveHook(track_sequence, "close");
         isUnderPrevBar = true;
         shouldChangeTrack = true;
         baseHasArrived = false;
         bar_count = 0;
-        Serial.println("detects correct bar, close hook & change track (RESET)");
         return;
     } else moveTrack(track_sequence, true);                                      // move current track
 
-    delay(15);
+    delay(50);
 }
